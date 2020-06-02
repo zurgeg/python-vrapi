e5f29322f3acf1dd59a7c865d2d2a46dc217c8a3
zurgeg <46549042+zurgeg@users.noreply.github.com>
2020-06-02 10:32:44 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from distutils.core import setup, Extension
-vrapi = Extension('vrapi', sources=['VrApi.h','VrApi_Config.h','VrApi_Helpers.h','VrApi_Input.h','VrApi_SystemUtils.h','VrApi_Types.h','VrApi_Vulkan.h','VrApi_Version.h'])
+vrapi = Extension('vrapi', sources=['bridge.c'])
 setup(name = 'oqvr',
        version = '1.0',
        description = 'A port of the Oculus Quest SDK',
