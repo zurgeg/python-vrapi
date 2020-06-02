0c3a5541db1b23d32fed70d2fe747529c41a37da
zurgeg <46549042+zurgeg@users.noreply.github.com>
2020-06-02 10:32:21 -0400

    Create bridge.c

---
+++ bridge.c
@@ -0,0 +1,11 @@
+#include "VrApi.h"
+#include "VrApi_Config.h"
+#include "VrApi_Helpers.h"
+#include "VrApi_Input.h"
+#include "VrApi_SystemUtils.h"
+#include "VrApi_Types.h"
+#include "VrApi_Version.h"
+#include "VrApi_Vulkan.h"
+int main(){
+    return 0;
+};
